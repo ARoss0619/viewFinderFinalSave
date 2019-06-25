86ee73bf056fc696f10e021abfcfa8fb8f76e0e4
Amber Ross <ross.amber1540@gmail.com>
2019-06-25 14:07:37 -0400

    complete interation 4

---
+++ AddPhotoViewController.swift
@@ -0,0 +1,57 @@
+//
+//  AddPhotoViewController.swift
+//  viewFinder
+//
+//  Created by Apple on 6/25/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+    
+    
+    @IBAction func takePhoto(_ sender: UIButton) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var newImage: UIImageView!
+    
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            newImage.image = selectedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func SaveAdd(_ sender: UIButton) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            let photoToSave = Photos(entity: Photos.entity(), insertInto: context)
+            
+            photoToSave.caption = captionText.text
+    
+            if let userImage = newImageView.image {
+                if let userImageData = userImage.pngData() {
+                    photoToSave.imageData = userImageData
+                }
+            }
+        }
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        
+        navigationController?.popViewController(animated: true)
+        
+    }
+    
+    @IBOutlet weak var newImageView: UIImageView!
+    
+    @IBOutlet weak var captionText: UITextField!
+    
+}
